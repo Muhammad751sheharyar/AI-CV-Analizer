b31f0a3801169f444b94d0d4185f71cd0de46b86
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-12-07 02:33:43 +0500

    first

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(__dirname));
+require('dotenv').config();
+
+
+// ---- Gemini 2.0 Flash Model ----
+const genAI = new GoogleGenerativeAI(process.env.Gemini_key);
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash",
+});
+
+app.post("/analyze", async (req, res) => {
+  try {
+    console.log("API HIT HUA");
+    console.log("BODY:", req.body);
+
+    const { resumeText, jobDescription } = req.body;
+
+    if (!resumeText || !jobDescription) {
+      return res.json({ error: "Resume or Job Description missing!" });
+    }
+
+    const prompt = `
+    Compare this resume with the job description.
+    Give:
+    - Match percentage (0-100%)
+    - Strengths
+    - Weaknesses
+    - Final verdict
+
+    Resume:
+    ${resumeText}
+
+    Job Description:
+    ${jobDescription}
+    `;
+
+    const result = await model.generateContent(prompt);
+
+    res.json({
+      result: result.response.text(),
+    });
+
+  } catch (err) {
+    console.error("ERROR:", err);
+    res.json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
+
+
+ 
+
+
+
